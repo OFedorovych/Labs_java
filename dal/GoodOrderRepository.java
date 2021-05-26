8d902f80e0e127814019e1872f6231de16d51e7e
OFedorovych <74896929+OFedorovych@users.noreply.github.com>
2021-05-27 00:33:22 +0300

    Delete GoodOrderRepository.java

--- dal/GoodOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.lviv.iot.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.lviv.iot.Good;
-
-public interface GoodOrderRepository extends JpaRepository<Good, Integer> {
-
-}
