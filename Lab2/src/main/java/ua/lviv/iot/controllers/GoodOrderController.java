5f410530c1d28036d80163dda6b7a5b70db2fabc
OFedorovych <74896929+OFedorovych@users.noreply.github.com>
2021-05-27 00:16:10 +0300

    Update Controller for lab10
    Add loggers and an exception

--- Lab2/src/main/java/ua/lviv/iot/controllers/GoodOrderController.java
+++ Lab2/src/main/java/ua/lviv/iot/controllers/GoodOrderController.java
@@ -6,20 +6,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import ua.lviv.iot.Good;
+import ua.lviv.iot.exceptions.OrderNotFoundException;
 import ua.lviv.iot.service.GoodOrderService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(path = "/good")
 public class GoodOrderController {
+    private static final Logger LOGGER = Logger.getLogger("ua.lviv.iot.controllers.GoodOrderController");
 
     @Autowired
     private GoodOrderService goodOrderService;
 
     @GetMapping(path = "/{id}")
-    public Good getGood(@PathVariable(name = "id") Integer id) {
-        return goodOrderService.getGood(id);
+    public ResponseEntity<Good> getGood(@PathVariable(name = "id") Integer id) {
+        try {
+            return new ResponseEntity<Good>(goodOrderService.getGood(id), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            LOGGER.severe("Can't update an order with non-existing id" + id);
+            return new ResponseEntity<Good>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping
@@ -28,29 +37,25 @@ public class GoodOrderController {
     }
 
     @PutMapping
-    public Good createGood(@RequestBody Good good) {
-        return goodOrderService.addGood(good);
+    public ResponseEntity<Good> createGood(@RequestBody Good good) {
+        if (good.getId() == null) {
+            return new ResponseEntity<Good>(goodOrderService.addGood(good), HttpStatus.OK);
+        }
+        LOGGER.severe("Failed to create an order with passed id. GoodOrder should not use external ids");
+        return new ResponseEntity<Good>(HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping
     public ResponseEntity<Good> updateGood(@RequestBody Good good) {
-        if (goodOrderService.getGood(good.getId()) != null) {
+        if(good.getId() == null){
+            LOGGER.severe("Can't update order without id - null value was passed instead of it");
+            return new ResponseEntity<Good>(HttpStatus.BAD_REQUEST);
+        }
+        try {
             return new ResponseEntity<Good>(goodOrderService.updateGood(good), HttpStatus.OK);
-        } else {
+        } catch (OrderNotFoundException e) {
+            LOGGER.severe("Can't update an order with non-existing id: " + good.getId());
             return new ResponseEntity<Good>(HttpStatus.NOT_FOUND);
         }
     }
-    @DeleteMapping(path = "/{id}")
-    public Good deleteGood(@PathVariable(name = "id") Integer id) {
-        return goodOrderService.delGood(id);
-    }
-//    @DeleteMapping
-//    public ResponseEntity<Good> deleteGood(@RequestBody Good good) {
-//        if (goodOrderService.getGood(good.getId()) != null) {
-//            goodOrderService.delGood(good);
-//            return new ResponseEntity<Good>(HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<Good>(HttpStatus.NOT_FOUND);
-//        }
-//    }
 }
